3f63cbae57cc124f6f98d422c194544d288aebae
Tina Liu <tina.chang.liu@gmail.com>
2019-10-28 13:52:26 +0000

    added solution

--- school.py
+++ school.py
@@ -1,1 +1,19 @@
-class School:
+class School():
+  def __init__(self, name, roster = {}):
+    self.name = name
+    self.roster = roster
+
+  def add_student(self, name, grade):
+    if grade in self.roster.keys():
+      self.roster[grade].append(name)
+    else:
+      self.roster[grade] = [name]
+
+  def grade(self, grade):
+    return self.roster[grade]
+
+  def sort_roster(self):
+    for key in self.roster:
+      self.roster[key].sort()
+    return self.roster
+
